dfa70026dfd1fd83308b2075d96d88f785cb721c
Brian Abidin <Brian.abidin1@gmail.com>
2023-02-09 15:35:12 -0500

    deleted unnecessary comments

--- tictactoe.js
+++ tictactoe.js
@@ -30,8 +30,6 @@ function newPlayer() {
   return {
     player1,
     player2
-    // name: `${name}`
-    // symbol: `${symbol}`
   };
 }
 
@@ -401,19 +399,3 @@ const gameController = (() => {
     reset
   };
 })();
-
-// store gameboard as an array in side of gameboard object
-// 4 main objects: Gameboard, Player 1, Player 2,
-// and control flow of game
-// have as little global code as possible.
-// when you need multiples of something, create factories
-// when you need 1 of something, create modules
-// write a javascript function that renders the contents of the
-// gameboard array to the webpage.
-// build functions that allows players to mark specific spots
-// tie the spots to the DOM, letting players click on gameboard to
-// mark. Once a spot is taken cannot be marked.
-// check for when game is over, check for 3 in a row.
-// interface: allow players to put their name, 2 buttons
-// start and restart. and a display to congratulate winner.
-// function factory creates objects as new players
